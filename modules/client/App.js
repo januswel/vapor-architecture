87bc202633793571e0d5d1d0b2fd9a00011d7a97
Takagi Kensuke <janus.wel.3@gmail.com>
2018-12-14 20:28:49 +0900

    :wrench::heavy_plus_sign: import domain to react native

--- modules/client/App.js
+++ modules/client/App.js
@@ -1,33 +1,12 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- */
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+import sum from 'vapor-domain'
 
-import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
-
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
-
-type Props = {};
-export default class App extends Component<Props> {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
-      </View>
-    );
-  }
-}
+export default () => (
+  <View style={styles.container}>
+    <Text style={styles.text}>{sum(1, 3)}</Text>
+  </View>
+)
 
 const styles = StyleSheet.create({
   container: {
@@ -36,14 +15,7 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     backgroundColor: '#F5FCFF',
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
+  text: {
+    fontSize: 32,
   },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
+})
